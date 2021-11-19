ff3edfdfc92ff3791a8376727dfcacead8440cbe
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-11-19 17:02:58 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -149,7 +149,7 @@ bot.loop.run_until_complete(assistants())
 bot.loop.run_until_complete(spams())
 
 print(
-    f"""♥️🇮🇳♥️⚜♥️
+    f"""
 ╔════❰LEGENDBOT❱═❍⊱❁۪۪
 ║┣⪼ OWNER - {Config.ALIVE_NAME}
 ║┣⪼ Group - @Legend_Userbot
@@ -171,7 +171,7 @@ async def legend_op():
             await bot.send_file(
                 Config.LOGGER_ID,
                 LEGEND_PIC,
-                caption=f"#START \nDeployed LEGENDBOT Successfully\n\n**LEGENDBOT- {LEGENDversion}**\n\nType `{l1}help` or `{l1}ping` to check! \n\nJoin [LegendBot Channel](t.me/Official_LegendBot) for Updates & [LegendBot Chat](t.me/Legend_Userbot) for any query regarding LegendBot",
+                caption=f"Deployed Lêɠêɳ̃dẞø† Successfully\n\nLêɠêɳ̃dẞø† ~ {LEGENDversion}**\n\nType `{l1}help` or `{l1}ping` to check!\nFor Assistant Type `.on` \n\nJoin [LegendBot Channel](t.me/Official_LegendBot) for Updates & [LegendBot Chat](t.me/Legend_Userbot) for any query regarding LegendBot",
             )
     except Exception as e:
         print(str(e))
