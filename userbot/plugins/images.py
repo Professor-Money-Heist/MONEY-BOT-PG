96f46c32ec3d444bdd0b848c6e7b8e89bdd4c0f1
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-11-22 16:56:36 +0530

    Update images.py

--- userbot/plugins/images.py
+++ userbot/plugins/images.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 from userbot.Config import Config
-from userbot.helpers.google_imgs import googleimagesdownload
+from LegendBot.helpers.google_imgs import googleimagesdownload
 from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
