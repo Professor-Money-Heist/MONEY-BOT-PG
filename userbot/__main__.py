8fee3965211b86934882d9880e2ca7fa10d562de
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-11-25 01:12:03 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -9,7 +9,7 @@ from telethon.tl.functions.channels import JoinChannelRequest
 
 from LEGENDBOY import lnbot
 from userbot import LOGS, LEGENDversion, bot
-from userbot.assistant.start import *
+from userbot.plugins.assistant.start import *
 from userbot.Config import Config
 from userbot.utils import (
     load_abuse,
