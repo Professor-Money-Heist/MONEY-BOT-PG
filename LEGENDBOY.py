7dc1844bd2c9460f81dbfd2ec644fdc28b8bdd17
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-11-25 02:07:49 +0530

    Update LEGENDBOY.py

--- LEGENDBOY.py
+++ LEGENDBOY.py
@@ -1,7 +1,7 @@
 import os
-
+from telethon.sessions import StringSession
 from telethon import TelegramClient
-
+from userbot.Config import Config 
 try:
     from userbot import bot
 except:
@@ -11,6 +11,40 @@ API_ID = os.environ.get("APP_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 token = os.environ.get("BOT_TOKEN", None)
 lnbot = TelegramClient("legendboy", API_ID, API_HASH).start(bot_token=token)
+
+class LegendBoy (TelegramClient):
+  def __init__(self, string, api_id, api_hash):
+    super().__init__(StringSession (string), api_id, api_hash)
+    self.id = None
+    self.username = None
+    self.bot_username = None
+    self.bot_token = None
+    self.heroku_username = None
+  def set(self, **u):
+    if u.get("bot_username"):
+      self.bot_username = u["bot_username"]
+    if u.get("id"):
+      self.id = u["id"]
+    if u.get("username"):
+      self.username = u["username"]
+    if u.get("bot_token"):
+      self.bot_token = u["bot_token"]
+    if u.get("heroku_username"):
+      self.heroku_username = u["heroku_username"]
+  def __str__(self):
+    detail = f'''
+Your name is {self.me.first_name}
+Your username is @{self.me.username or "no Username"}
+Your bot Username is @{self.bot_username}
+Your heroku bot username is {self.heroku_username}'''
+    return detail
+
+if Config.LEGEND_STRING:
+    session_name = str(Config.LEGEND_STRING)
+    bbbot = LegendBoy(session_name, Config.APP_ID, Config.API_HASH)
+else:
+    session_name = "startup"
+    bbbot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
 
 if __name__ == "__main__":
     bot.start()
