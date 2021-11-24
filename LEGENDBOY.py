43f00296d6d8dfb94f819adbe107a1523a014b2c
LEGEND-OS <Krishna045Jaiswal@gmail.com>
2021-11-24 18:11:14 +0000

    auto fixes

--- LEGENDBOY.py
+++ LEGENDBOY.py
@@ -1,19 +1,18 @@
-import asyncio
 import os
-import asyncio
+
 from telethon import TelegramClient
-from telethon.sessions import StringSession
+
 try:
-  from userbot import bot 
+    from userbot import bot
 except:
-  pass
+    pass
 
 API_ID = os.environ.get("APP_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 token = os.environ.get("BOT_TOKEN", None)
 lnbot = TelegramClient("legendboy", API_ID, API_HASH).start(bot_token=token)
 
-if __name__=="__main__":
-  bot.start()
-  bot.run_until_disconnected()
-  lnbot.run_until_disconnected()
+if __name__ == "__main__":
+    bot.start()
+    bot.run_until_disconnected()
+    lnbot.run_until_disconnected()
