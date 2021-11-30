b03f94c93dbb2ef2341e326c63e418f9ccf4ea79
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-11-30 08:55:37 +0530

    Update ping.py

--- userbot/plugins/assistant/ping.py
+++ userbot/plugins/assistant/ping.py
@@ -3,7 +3,7 @@ import os
 from userbot import *
 from userbot.Config import Config
 from userbot.plugins import *
-
+from LEGENDBOY import lnbot
 LEGEND_IMG = os.environ.get(
     "BOT_PING_PIC", "https://telegra.ph/file/a9f6a3c160977352dd595.jpg"
 )
@@ -13,7 +13,7 @@ LEGEND_IMG = os.environ.get(
 async def _(event):
     ms = 4
     ALIVE = Config.ALIVE_NAME
-    await tgbot.send_file(
+    await lnbot.send_file(
         event.chat_id,
         LEGEND_IMG,
         f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   ⚜ {ms}\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{ALIVE}』",
