ee8e5970e881b5956f47a69232e2260e789e4c1e
LEGEND-OS <Krishna045Jaiswal@gmail.com>
2021-11-30 03:41:12 +0000

    auto fixes

--- userbot/plugins/assistant/ping.py
+++ userbot/plugins/assistant/ping.py
@@ -1,6 +1,5 @@
 import os
 
-from LEGENDBOY import lnbot
 from userbot import *
 from userbot.Config import Config
 from userbot.plugins import *
@@ -11,14 +10,10 @@ LEGEND_IMG = os.environ.get(
 ms = 4
 ALIVE = Config.ALIVE_NAME
 
-LegendBoy=f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   ⚜ {ms}\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{ALIVE}』",
-    
+LegendBoy = (f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   ⚜ {ms}\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{ALIVE}』",)
+
+
 @tgbot.on(events.NewMessage(pattern="^/ping"))
 async def _(event):
     GOOD = [[Button.url(" Owner ", "https://t.me/The_LegendBoy")]]
-    await tgbot.send_file(
-        event.chat_id,
-        LEGEND_IMG,
-        caption=LegendBoy,
-        buttons=GOOD
-        )
+    await tgbot.send_file(event.chat_id, LEGEND_IMG, caption=LegendBoy, buttons=GOOD)
