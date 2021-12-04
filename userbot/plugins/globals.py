19782e692d03f21b51ab1318c1fd93457da58ac8
LEGEND-OS <Krishna045Jaiswal@gmail.com>
2021-12-04 09:37:06 +0000

    auto fixes

--- userbot/plugins/globals.py
+++ userbot/plugins/globals.py
@@ -390,8 +390,10 @@ async def gm(event):
     elif private is True:
         userid = event.chat_id
     else:
-        return await eod(event, "Need a user to gmute. Reply or give userid to gmute them..")
-    name = (await event.client.get_entity(userid)).first_name
+        return await eod(
+            event, "Need a user to gmute. Reply or give userid to gmute them.."
+        )
+    (await event.client.get_entity(userid)).first_name
     event.chat_id
     await event.get_chat()
     if gsql.is_gmuted(userid, "gmute"):
@@ -407,10 +409,18 @@ async def gm(event):
         await eod(event, "Error occured!\nError is " + str(e))
     else:
         if Config.ABUSE == "ON":
-            await bot.send_file(event.chat_id, LEGEND_logo1, caption=f"**(~‾▿‾)~ Chup [Madarchod](tg://user?id={userid}) ....**", reply_to=reply)
+            await bot.send_file(
+                event.chat_id,
+                LEGEND_logo1,
+                caption=f"**(~‾▿‾)~ Chup [Madarchod](tg://user?id={userid}) ....**",
+                reply_to=reply,
+            )
             await event.delete()
         else:
-            await eor(event, "**Globally Muted [{name}](tg://user?id={userid}) !!**\n\n__Kid struggling to speak__ ♪～(´ε｀ )")
+            await eor(
+                event,
+                "**Globally Muted [{name}](tg://user?id={userid}) !!**\n\n__Kid struggling to speak__ ♪～(´ε｀ )",
+            )
 
 
 @bot.on(admin_cmd(outgoing=True, pattern=r"ungmute ?(\d+)?"))
@@ -431,8 +441,10 @@ async def gm(event):
     elif private is True:
         userid = event.chat_id
     else:
-        return await eod(event, "Need a user to gmute. Reply or give userid to gmute them..")
-    name = (await event.client.get_entity(userid)).first_name
+        return await eod(
+            event, "Need a user to gmute. Reply or give userid to gmute them.."
+        )
+    (await event.client.get_entity(userid)).first_name
     event.chat_id
     await event.get_chat()
     if gsql.is_gmuted(userid, "gmute"):
@@ -448,10 +460,18 @@ async def gm(event):
         await eod(event, "Error occured!\nError is " + str(e))
     else:
         if Config.ABUSE == "ON":
-            await bot.send_file(event.chat_id, shhh, caption=f"**(~‾▿‾)~ Chup [Madarchod](tg://user?id={userid}) ....**", reply_to=reply)
+            await bot.send_file(
+                event.chat_id,
+                shhh,
+                caption=f"**(~‾▿‾)~ Chup [Madarchod](tg://user?id={userid}) ....**",
+                reply_to=reply,
+            )
             await event.delete()
         else:
-            await eor(event, "**Globally Muted [{name}](tg://user?id={userid}) !!**\n\n__Kid struggling to speak__ ♪～(´ε｀ )")
+            await eor(
+                event,
+                "**Globally Muted [{name}](tg://user?id={userid}) !!**\n\n__Kid struggling to speak__ ♪～(´ε｀ )",
+            )
 
 
 @command(incoming=True)
