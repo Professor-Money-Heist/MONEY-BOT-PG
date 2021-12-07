8d0e44a4fc3d74b58d9036e3d8342607cb4c0e45
Professor Agora <agoraprofessor@gmail.com>
2021-12-07 12:00:30 +0530

    Update raid.py

--- userbot/plugins/Spam/raid.py
+++ userbot/plugins/Spam/raid.py
@@ -425,10 +425,10 @@ async def _(e):
     if ABUSE == "ON":
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None)
-        legend = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        PROFAGORA = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
         await e.get_reply_message()
         if len(e.text) > 11:
-            message = str(legend[0])
+            message = str(PROFAGORA[0])
             a = await e.client.get_entity(message)
             g = a.id
             que[g] = []
