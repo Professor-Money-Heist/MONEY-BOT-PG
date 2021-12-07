5846aaa03566b10fb8be9b04f4f50bcb4c6733a0
Professor Agora <agoraprofessor@gmail.com>
2021-12-07 11:53:11 +0530

    Update delayspam.py

--- userbot/plugins/Spam/delayspam.py
+++ userbot/plugins/Spam/delayspam.py
@@ -27,12 +27,13 @@ async def spam(e):
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None)
         smex = await e.get_reply_message()
-        legendboy = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
-        legendgirl = Ustad[1:]
-        if len(legendgirl) == 2:
-            message = str(legendgirl[1])
-            counter = int(legendgirl[0])
-            sleeptime = float(legendboy[0])
+        ProfAgora = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
+        ProfAgora = Ustad[1:]
+        if len(leg
+            (ProfAgora) == 2:
+            message = str(ProfAgora[1])
+            counter = int(ProfAgora[0])
+            sleeptime = float(ProfAgora[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "typing"):
                     if e.reply_to_msg_id:
@@ -41,8 +42,8 @@ async def spam(e):
                         await e.client.send_message(e.chat_id, message)
                     await asyncio.sleep(sleeptime)
         elif e.reply_to_msg_id and smex.media:
-            counter = int(legendgirl[0])
-            sleeptime = float(legendboy[0])
+            counter = int(ProfAgora[0])
+            sleeptime = float(ProfAgora[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "document"):
                     smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
@@ -50,8 +51,8 @@ async def spam(e):
                 await asyncio.sleep(sleeptime)
         elif e.reply_to_msg_id and smex.text:
             message = smex.text
-            counter = int(legendgirl[0])
-            sleeptime = float(legendboy[0])
+            counter = int(ProfAgora[0])
+            sleeptime = float(PtofAgora[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "typing"):
                     await e.client.send_message(e.chat_id, message)
