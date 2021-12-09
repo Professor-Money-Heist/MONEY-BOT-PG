3f0b35600a7a871aba14ff7c1ca9fd6589dd2e22
Professor Agora <agoraprofessor@gmail.com>
2021-12-09 23:08:27 +0530

    Update PROFESSORString.py

--- PROFESSORString.py
+++ PROFESSORString.py
@@ -15,7 +15,7 @@ if okvai == "69":
         client.send_message("me", client.session.save())
         client.send_message(
             "me",
-            "Above is your #PROFESSOR STRING SESSION \nPaste this string in Heroku Var.\n\n[SOLO PROFESSOR AGORA](t.me/MM_USERBOT)",
+            "Above is your #PROFESSOR-STRING-SESSION \nPaste this string in Heroku Var.\n\n[PROFESSOR](t.me/MM_USERBOT)",
         )
 
 else:
