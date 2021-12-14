4261e12b621484543f2938dac5ca09fc0f5647a3
Professor Agora <agoraprofessor@gmail.com>
2021-12-14 10:29:12 +0530

    Update listcmnd.py

--- userbot/plugins/listcmnd.py
+++ userbot/plugins/listcmnd.py
@@ -1,18 +1,18 @@
 import asyncio
 import io
 
-from LEGENDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+from MONEY-HEIST-BOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot import ALIVE_NAME
-from userbot import bot as LEGENDBOT
+from userbot import bot as MONEY-HEIST-BOT
 
 from . import *
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND"
-LEGEND_logo = "LEGEND_logo1"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "MONEY"
+MONEY_logo = "MONEY_logo1"
 
 
-@LEGENDBOT.on(admin_cmd(pattern=r"listcmnd"))
-@LEGENDBOT.on(sudo_cmd(pattern=r"listcmnd", allow_sudo=True))
+@MONEY_HEIST_BOT.on(admin_cmd(pattern=r"listcmnd"))
+@MONEY_HEIST_BOT.on(sudo_cmd(pattern=r"listcmnd", allow_sudo=True))
 async def install(event):
     if event.fwd_from:
         return
@@ -20,7 +20,7 @@ async def install(event):
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
     cmd = "ls userbot/plugins"
-    thumb = LEGEND_logo
+    thumb = MONEY_logo
     process = await asyncio.create_subprocess_shell(
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
@@ -28,11 +28,11 @@ async def install(event):
     o = stdout.decode()
     _o = o.split("\n")
     o = "\n".join(_o)
-    OUTPUT = f"List of Plugins in LEGENDBOT :- \n\n{o}\n\n<><><><><><><><><><><><><><><><><><><><><><><><>\nHELP:- If you want to know the commands for a plugin, do :- \n.plinfo <plugin name> without the < > brackets. \nJoin https://t.me/LEGENDSupport for help."
+    OUTPUT = f"List of Plugins in MONEY-HEIST-BOT :- \n\n{o}\n\n<><><><><><><><><><><><><><><><><><><><><><><><>\nHELP:- If you want to know the commands for a plugin, do :- \n.plinfo <plugin name> without the < > brackets. \nJoin https://t.me/LEGENDSupport for help."
     if len(OUTPUT) > 69:
         with io.BytesIO(str.encode(OUTPUT)) as out_file:
             out_file.name = "cmd_list.text"
-            LEGEND_file = await bot.send_file(
+            MONEY_file = await bot.send_file(
                 event.chat_id,
                 out_file,
                 force_document=True,
@@ -41,8 +41,8 @@ async def install(event):
                 reply_to=reply_to_id,
             )
             await edit_or_reply(
-                LEGEND_file,
-                f"Output Too Large. This is the file for the list of plugins in LEGENDBOT.\n\n**BY :-** {DEFAULTUSER}",
+                MONEY_file,
+                f"Output Too Large. This is the file for the list of plugins in MONEY-HEIST-BOT.\n\n**BY :-** {DEFAULTUSER}",
             )
             await event.delete()
 
